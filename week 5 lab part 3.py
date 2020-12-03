e2c0a1f681325b1811a9764744b51f74157986e0
jasmineirizarry <72569201+jasmineirizarry@users.noreply.github.com>
2020-12-03 14:16:39 -0600

    Add files via upload

---
+++ week 5 lab part 3.py
@@ -0,0 +1,27 @@
+#jasmine irizarry
+#week 5 lab assignment
+#CSS225
+#part 3
+
+import turtle
+polygon =turtle.Turtle()
+
+pen_color = input ("Enter the pen color: ")
+fill_color = input ("Enter fill color: ")
+
+t.color(pen_color, fill_color)
+
+t.begin_fill()
+for i in range(6):
+    polygon.forward(100)
+    polygon.right(60)
+t.end_fill()
+
+#I KEEP GETTING THIS ERROR AFTER FIXING IT MULTIPLE TIMES:
+
+#line 12, in <module>
+#t.color(pen_color, fill_color)
+#NameError: name 't' is not defined
+
+
+
---
+++ week 5 practice with turtle.py
@@ -0,0 +1,11 @@
+#jasmine irizarry
+#week 5 lab assignment
+#CSS225
+
+
+#part 1
+
+print ("hello world"*100)
+
+
+
